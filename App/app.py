30ffb905c61640690626ea2ac25ff4fce03373b7
Anishfathima <anishfathima.thrive@gmail.com>
2026-03-12 00:38:39 +0530

    fix: use environment variable for mail password

--- App/app.py
+++ App/app.py
@@ -15,7 +15,7 @@ app.config['MAIL_SERVER']         = 'smtp.gmail.com'
 app.config['MAIL_PORT']           = 587
 app.config['MAIL_USE_TLS']        = True
 app.config['MAIL_USERNAME']       = 'anishansarbasha@gmail.com'  # Your Gmail
-app.config['MAIL_PASSWORD']       = 'pbfp vgzo izfh efbf'    # Replace with Gmail App Password
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')   # Replace with Gmail App Password
 app.config['MAIL_DEFAULT_SENDER'] = 'anishansarbasha@gmail.com'
 
 mail = Mail(app)
